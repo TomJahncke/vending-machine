7f1a5b002c0bb076385dfb5d142e90f9ae27172b
Brentt Smith <brentt.smith@gfs.com>
2021-02-02 09:21:58 -0500

    Adding an empty test case as place holder

---
+++ src/test/java/com/gfs/vendingmachine/VendingMachineImplTest.java
@@ -0,0 +1,14 @@
+package com.gfs.vendingmachine;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VendingMachineImplTest {
+
+
+    @Test
+    public void testSomething() {
+        // here is where you implement some tests
+    }
+}
